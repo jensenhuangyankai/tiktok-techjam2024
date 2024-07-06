ebef019796fca36502a5c2e0e6670cf091e8f64c
Jensen <jensen@huangyankai.com>
2024-07-06 18:21:18 +0800

    changes

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,15 @@
-from flask import Flask
+from flask import Flask, request
+from os.path import isfile, join
+from mimetypes import MimeTypes
+from os import listdir
+import hashlib
+import json
+import hmac
+import copy
+import sys
+import os
+
+
 
 app = Flask(__name__)
 
@@ -6,3 +17,5 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
+@app.route("/getTags", methods = ["POST"])
+def getTags():
